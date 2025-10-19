eb9ccc881efa877654ced5e595c622e4ab6c9fb1
ptesny <rth@tiosouji.com>
2025-10-19 19:02:06 +0100

    Update provider.tf

--- exercises/ex1/k8s-mt-data/provider.tf
+++ exercises/ex1/k8s-mt-data/provider.tf
@@ -4,26 +4,7 @@ terraform {
     btp = {
       source  = "SAP/btp"
     }
-    jq = {
-      source  = "massdriver-cloud/jq"
-    }
-    http-full = {
-      source = "salrashid123/http-full"
-    }     
-    qrcode = {
-      source = "jackivanov/qrcode"
-      //version = "1.0.0"
-    }
   }
-}
-
-provider "jq" {}
-provider "http-full" {}
-
-# https://registry.terraform.io/providers/jackivanov/qrcode/latest/docs
-#
-provider "qrcode" {
-  # Configuration options
 }
 
 provider "btp" {
